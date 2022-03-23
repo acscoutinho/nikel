0120c7fbfff1a356bdbb6630186c961c33a3cf2d
perkles <acscoutinhodba@gmail.com>
2022-03-23 14:06:56 -0300

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+const myModal = new bootstrap.Modal("#register-modal");
+
+//criar coonta
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    if(email.length > 20) {
+        alert("Preencha o campo com um e-mail válido.")
+        return;
+    }
+
+    if(password.length < 4) {
+        alert("Preencha a senha com no mínimo 4 digitos.")
+        return;
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+    myModal.hide();
+
+    alert("Conta criada com sucesso.");
+});
+
+function saveAccount(data) {
+   localStorage.setItem(data.login, JSON.stringify(data));
+}
